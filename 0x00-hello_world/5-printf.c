d844c2ea0ba3b65d78963f37eaca229ee30ab382
Hope <hopemotchesane@gmail.com>
2022-09-12 16:20:45 -0700

    compile using -wallgcc w/o warnings

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: "with proper grammer, but the outcome is a piece of art,"
+ * Description: prints "with proper grammer, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 { 
-	printf("with proper grammer,but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }

8310dbeff2af8da1a0f5317dc1827b84effb4604
Hope <hopemotchesane@gmail.com>
2022-09-21 18:54:46 -0700

    still crying

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,15 +6,15 @@
  * Return: 0
  */
 
-void print_alphabet(void),
-({
+int main(void)
+{
 	int i;
 
 	for (i = 'a' ; i <= 'z'; i++)
 
 	{
-		_putchar(i);
+		putchar(i);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }

8edf4fa2e45018a94b3eb9e8db2ba412636f31d9
Hope M <101661762+HopeM2@users.noreply.github.com>
2022-09-22 16:43:58 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
 int _putchar(char);
 
 void print_alphabet(void);
@@ -21,3 +24,4 @@ void times_table(void);
 void print_to_98(int n);
 
 void print_times_table(int n);
+#endif

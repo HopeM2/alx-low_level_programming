79ab423b3fbeb3252673a0c9972fa3828b3d6349
Hope <hopemotchesane@gmail.com>
2022-09-22 08:03:31 -0700

    i tried (-wimplicit-function-declaration)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for(1 == 0; i <= 9; i++)
+
+	{
+		for (c = 'a'; c<= 'z'; c++)
+		{
+			putchar(c);
+		}
+	putchar('\n');
+	i++;
+
+	}
+	
+}
